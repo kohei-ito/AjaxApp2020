1ca398402ce1d2d7ee87aa8121c37b3b7d67020d
kohei-ito <kohei.ito@di-v.co.jp>
2020-09-27 20:40:44 +0900

    update 既読機能

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,4 +1,5 @@
 function check() {
+  // 表示されているすべてのメモを取得している
   const posts = document.getElementsByClassName("post");
 
   postsA = Array.from(posts);
@@ -9,9 +10,12 @@ function check() {
     }
     post.setAttribute("data-load", "true");
 
+    // メモをクリックした場合に実行する処理を定義している
     post.addEventListener("click", (e) => {
+      // どのメモをクリックしたのか、カスタムデータを利用して取得している
       const postId = post.getAttribute("data-id");
 
+      // Ajaxに必要なオブジェクトを生成している
       const XHR = new XMLHttpRequest();
 
       // openでリクエストを初期化する
@@ -25,19 +29,22 @@ function check() {
 
       // レスポンスを受け取った時
       XHR.onload = () => {
+        if (XHR.status != 200) {
+          // レスポンスの HTTP ステータスを解析し、該当するエラーメッセージをアラートで表示するようにしている
+          alert(`Error ${XHR.status}: ${XHR.statusText}`);
+          //処理の終了
+          return null;
+        }
+        // レスポンスされたデータを変数itemに代入している
         const item = XHR.response.post;
         if (item.checked === true) {
+          // 既読状態であれば、灰色に変わるcssを適用するためのカスタムデータを追加している
           post.setAttribute("data-check", "true");
         } else if (item.checked === false) {
+          // 未読状態であれば、カスタムデータを削除している
           post.removeAttribute("data-check");
         }
 
-        if (XHR.status != 200) {
-          // レスポンスの HTTP ステータスを解析
-          alert(`Error ${XHR.status}: ${XHR.statusText}`); // e.g. 404: Not Found
-        } else {
-          return null;
-        }
       };
       // リクエストが送信できなかった時
       XHR.onerror = function () {
